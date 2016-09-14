0d92cc0824d40db010b8e7e02f9b866d82c097eb
Ghislain Rodrigues <git@ghislain-rodrigues.fr>
2016-09-14 23:19:12 +0100

    grid rendered when needed

--- main.c
+++ main.c
@@ -3,6 +3,8 @@
 
 #define WIDTH_GRID 14
 #define HEIGHT_GRID 14
+#define WIDTH_GRID_PX 17
+#define HEIGHT_GRID_PX 17
 #define NB_COLORS 6
 
 /**
@@ -27,6 +29,7 @@ int g_colors[NB_COLORS][3] = {
 
 int initSDL(const char* title, const int x, const int y, const int w, const int h);
 void generateGrid();
+void renderGrid();
 
 int main()
 {
@@ -38,7 +41,7 @@ int main()
 	generateGrid();
 
 	// program main loop
-	int done = 0;
+	int done = 0, needsRefresh = 1;
 	while (!done) {
 		// message processing loop
 		SDL_Event event;
@@ -61,29 +64,10 @@ int main()
 		} // end of message processing
 
 		// DRAWING STARTS HERE
-
-		// Set render color to red (background will be rendered in this color)
-		SDL_SetRenderDrawColor(g_renderer, 0, 0, 0, 255);
-
-		// Clear winow
-		SDL_RenderClear(g_renderer);
-
-		// Creat a rect at pos (50, 50) that's 50 pixels wide and 50 pixels high.
-		SDL_Rect r;
-		r.x = 50;
-		r.y = 50;
-		r.w = 50;
-		r.h = 50;
-
-		// Set render color to blue (rect will be rendered in this color)
-		SDL_SetRenderDrawColor(g_renderer, 0, 0, 255, 255);
-
-		// Render rect
-		SDL_RenderFillRect(g_renderer, &r);
-
-		// Render the rect to the screen
-		SDL_RenderPresent(g_renderer);
-
+		if (needsRefresh) {
+			renderGrid();
+			needsRefresh = 0;
+		}
 		// DRAWING ENDS HERE
 	} // end main loop
 
@@ -126,6 +110,39 @@ void generateGrid() {
 	for (j = 0; j < HEIGHT_GRID; ++j){
 		for (i = 0; i < WIDTH_GRID; ++i){
 			g_grid[j][i] = rand() % NB_COLORS;
+		}
+	}
+}
+
+void renderGrid() {
+	// Set render color to red (background will be rendered in this color)
+	SDL_SetRenderDrawColor(g_renderer, 0, 0, 0, 255);
+
+	// Clear winow
+	SDL_RenderClear(g_renderer);
+	int i, j, margin = 1;
+	for (j = 0; j < HEIGHT_GRID; ++j){
+		for (i = 0; i < WIDTH_GRID; ++i){
+			// Creat a rect at pos (50, 50) that's 50 pixels wide and 50 pixels
+			// high.
+			SDL_Rect r;
+			int cR, cG,cB;
+			r.x = margin + i * WIDTH_GRID_PX;
+			r.y = margin + j * HEIGHT_GRID_PX;
+			r.w = WIDTH_GRID_PX;
+			r.h = HEIGHT_GRID_PX;
+			cR = g_colors[g_grid[j][i]][0];
+			cG = g_colors[g_grid[j][i]][1];
+			cB = g_colors[g_grid[j][i]][2];
+
+			// Set render color to blue (rect will be rendered in this color)
+			SDL_SetRenderDrawColor(g_renderer, cR, cG, cB, 255);
+
+			// Render rect
+			SDL_RenderFillRect(g_renderer, &r);
+
+			// Render the rect to the screen
+			SDL_RenderPresent(g_renderer);
 		}
 	}
 }
