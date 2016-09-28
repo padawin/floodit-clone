50fffccb3c1d5ae4a76c986f7b29e80c4a856966
Ghislain Rodrigues <git@ghislain-rodrigues.fr>
2016-09-28 07:46:18 +0100

    file opening moved in a separate function
    \n added

--- high_score.c
+++ high_score.c
@@ -5,7 +5,10 @@
 #include <string.h>
 #include "high_score.h"
 
+FILE* _open_file(const char *mode);
+
 void high_score_save(const int time, const int turns) {
+	FILE *f;
 	char filePath[255];
 
 	sprintf(
@@ -22,13 +25,23 @@ void high_score_save(const int time, const int turns) {
 		mkdir(filePath, 0700);
 	}
 
-	sprintf(filePath, "%s/%s", getenv("HOME"), HIGH_SCORES_LOCATION);
-	FILE *f = fopen(filePath, "a");
+	f = _open_file("a");
 	if (f == NULL) {
-		printf("Error opening file %s\n", filePath);
 		return;
 	}
 
 	fprintf(f, "%d %d\n", time, turns);
 	fclose(f);
+}
+
+FILE* _open_file(const char *mode) {
+	FILE *f;
+	char filePath[255];
+	sprintf(filePath, "%s/%s", getenv("HOME"), HIGH_SCORES_LOCATION);
+	f = fopen(filePath, mode);
+	if (f == NULL) {
+		printf("Error while reading high scores file %s\n", filePath);
+	}
+
+	return f;
 }
