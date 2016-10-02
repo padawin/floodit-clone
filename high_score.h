ccc4e43f41143334a92abab0992bde630036d14f
Ghislain Rodrigues <git@ghislain-rodrigues.fr>
2016-10-02 22:14:53 +0100

    max number of high scores reduced

--- high_score.h
+++ high_score.h
@@ -2,7 +2,7 @@
 #define __HIGH_SCORE__
 
 #define HIGH_SCORES_LOCATION ".floodit/highscores"
-#define MAX_HIGH_SCORES_NUMBER 10
+#define MAX_HIGH_SCORES_NUMBER 5
 
 void high_score_save(const int time, const int turns);
 void high_score_list(int *times, int *turns, int *nbRows);
