46c91abfe65add82211b514500337bd3298ada8c
Ghislain Rodrigues <git@ghislain-rodrigues.fr>
2016-10-04 21:55:07 +0100

    state set to the multiplayer setup

--- multiplayer_setup_state.c
+++ multiplayer_setup_state.c
@@ -5,8 +5,11 @@
 #include "multiplayer_setup_state.h"
 #include "utils.h"
 
-void multiplayer_setup_state_init(s_Game *game) {
+int STATE_HOST_JOIN = 1;
+int g_localState;
 
+void multiplayer_setup_state_init(s_Game *game) {
+	g_localState = STATE_HOST_JOIN;
 }
 
 void multiplayer_setup_state_clean(s_Game *game) {
