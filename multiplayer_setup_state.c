141a539839a5cf901cacdd4cdfb2f3e8d6d5e2a0
Ghislain Rodrigues <git@ghislain-rodrigues.fr>
2016-10-04 21:55:27 +0100

    menu added to host or join a game

--- multiplayer_setup_state.c
+++ multiplayer_setup_state.c
@@ -2,22 +2,51 @@
 #include <SDL2/SDL_ttf.h>
 #include "globals.h"
 #include "game.h"
+#include "menu.h"
 #include "multiplayer_setup_state.h"
 #include "utils.h"
+
+s_Menu g_hostJoinMenu;
 
 int STATE_HOST_JOIN = 1;
 int g_localState;
 
+void _initMenus();
+void _hostGameAction(s_Game *game);
+void _joinGameAction(s_Game *game);
+void _backAction(s_Game *game);
+void _renderHostJoinMenu();
+
 void multiplayer_setup_state_init(s_Game *game) {
+	_initMenus(game);
 	g_localState = STATE_HOST_JOIN;
 }
 
-void multiplayer_setup_state_clean(s_Game *game) {
+void _initMenus(s_Game *game) {
+	menu_setActionsNumber(&g_hostJoinMenu, 3);
+	SDL_Texture *hostGameTexture, *selectedHostGameTexture,
+		*joinGameTexture, *selectedJoinGameTexture,
+		*backTexture, *selectedBackTexture;
+	SDL_Color white = {255, 255, 255};
+	utils_createTextTexture(game->renderer, game->menuFont, "Host game", white, &hostGameTexture);
+	utils_createTextTexture(game->renderer, game->selectedMenuFont, "Host game", white, &selectedHostGameTexture);
+	utils_createTextTexture(game->renderer, game->menuFont, "Join game", white, &joinGameTexture);
+	utils_createTextTexture(game->renderer, game->selectedMenuFont, "Join game", white, &selectedJoinGameTexture);
+	utils_createTextTexture(game->renderer, game->menuFont, "Back", white, &backTexture);
+	utils_createTextTexture(game->renderer, game->selectedMenuFont, "Back", white, &selectedBackTexture);
+	menu_addAction(&g_hostJoinMenu, _hostGameAction, hostGameTexture, selectedHostGameTexture);
+	menu_addAction(&g_hostJoinMenu, _joinGameAction, joinGameTexture, selectedJoinGameTexture);
+	menu_addAction(&g_hostJoinMenu, _backAction, backTexture, selectedBackTexture);
+}
 
+void multiplayer_setup_state_clean(s_Game *game) {
+	menu_free(&g_hostJoinMenu);
 }
 
 void multiplayer_setup_render(s_Game* game) {
-	printf("Render multi setup\n");
+	if (g_localState == STATE_HOST_JOIN) {
+		menu_render(game, &g_hostJoinMenu);
+	}
 }
 
 void multiplayer_setup_handleEvent(s_Game* game, int key) {
@@ -25,4 +54,24 @@ void multiplayer_setup_handleEvent(s_Game* game, int key) {
 		multiplayer_setup_state_clean(game);
 		game_init(game);
 	}
+
+	if (g_localState == STATE_HOST_JOIN) {
+		menu_handleEvent(game, &g_hostJoinMenu, key);
+	}
+}
+
+void _hostGameAction(s_Game *game) {
+	printf("Host Game \n");
+}
+
+void _joinGameAction(s_Game *game) {
+	printf("Join Game \n");
+}
+
+void _backAction(s_Game *game) {
+	game_init(game);
+}
+
+void _renderHostJoinMenu() {
+
 }
