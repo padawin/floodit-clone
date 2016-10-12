2991efb19844df88327dac2dda68585a41dc9030
Ghislain Rodrigues <git@ghislain-rodrigues.fr>
2016-10-12 13:15:28 +0100

    the number of players is now used from the atlas
    texture freed

--- multiplayer_setup_state.c
+++ multiplayer_setup_state.c
@@ -9,6 +9,7 @@
 
 s_Menu g_hostJoinMenu;
 SDL_Color white = {255, 255, 255};
+SDL_Texture *selectPlayersTexture;
 SDL_Texture *selectPlayersNumberTexture;
 int g_playersNumber = 2;
 
@@ -42,11 +43,21 @@ void _initMenus(s_Game *game) {
 	menu_addAction(&g_hostJoinMenu, _hostGameAction, hostGameTexture, selectedHostGameTexture);
 	menu_addAction(&g_hostJoinMenu, _joinGameAction, joinGameTexture, selectedJoinGameTexture);
 	menu_addAction(&g_hostJoinMenu, _backAction, backTexture, selectedBackTexture);
+
+	utils_createTextTexture(
+		game->renderer,
+		game->menuFont,
+		"Players:",
+		white,
+		&selectPlayersTexture
+	);
+	utils_loadImageTexture(game->renderer, "resources/text-atlas.png", &selectPlayersNumberTexture);
 }
 
 void multiplayer_setup_state_clean(s_Game *game) {
 	menu_free(&g_hostJoinMenu);
 	SDL_DestroyTexture(selectPlayersNumberTexture);
+	SDL_DestroyTexture(selectPlayersTexture);
 }
 
 void multiplayer_setup_update(s_Game* game) {
@@ -60,16 +71,20 @@ void multiplayer_setup_render(s_Game* game) {
 		menu_render(game, &g_hostJoinMenu);
 	}
 	else if (g_localState == STATE_HOST_SETUP) {
-		if (selectPlayersNumberTexture != 0) {
-			SDL_DestroyTexture(selectPlayersNumberTexture);
-		}
-		char txt[19];
-		int textWidth, textHeight;
-		snprintf(txt, 19, "Players: < %d >", g_playersNumber);
-		utils_createTextTexture(game->renderer, game->menuFont, txt, white, &selectPlayersNumberTexture);
-		SDL_QueryTexture(selectPlayersNumberTexture, NULL, NULL, &textWidth, &textHeight);
+		int textWidth, textWidthNumber, textHeight;
+		SDL_QueryTexture(selectPlayersTexture, NULL, NULL, &textWidth, &textHeight);
 		SDL_Rect rect = {50, 30, textWidth, textHeight};
-		SDL_RenderCopy(game->renderer, selectPlayersNumberTexture, NULL, &rect);
+		SDL_RenderCopy(game->renderer, selectPlayersTexture, NULL, &rect);
+
+		SDL_QueryTexture(selectPlayersNumberTexture, NULL, NULL, &textWidthNumber, &textHeight);
+		SDL_Rect srcRect = {11 * (g_playersNumber - 2), 0, 11, textHeight};
+		SDL_Rect destRect = {55 + textWidth, 31, 11, textHeight};
+		SDL_RenderCopyEx(
+			game->renderer,
+			selectPlayersNumberTexture,
+			&srcRect, &destRect,
+			0, 0, 0
+		);
 	}
 }
 
@@ -94,10 +109,10 @@ void multiplayer_setup_handleEvent(s_Game* game, int key) {
 		else if (key == SDLK_ESCAPE) {
 			g_localState = STATE_HOST_JOIN;
 		}
-		else if (key == SDLK_RIGHT && g_playersNumber < MULTIPLAYER_MAX_PLAYERS_NUMBER) {
+		else if (key == SDLK_UP && g_playersNumber < MULTIPLAYER_MAX_PLAYERS_NUMBER) {
 			++g_playersNumber;
 		}
-		else if (key == SDLK_LEFT && g_playersNumber > MULTIPLAYER_MIN_PLAYERS_NUMBER) {
+		else if (key == SDLK_DOWN && g_playersNumber > MULTIPLAYER_MIN_PLAYERS_NUMBER) {
 			--g_playersNumber;
 		}
 	}
