f9344d2589202d3b0402854c8b49da5baa8800d1
Ghislain Rodrigues <git@ghislain-rodrigues.fr>
2016-10-23 11:03:45 +0100

    old comments removed

--- main.c
+++ main.c
@@ -58,15 +58,12 @@ int initSDL(const char* title, const int x, const int y, const int w, const int 
 		flags = 0;
 	}
 
-	// initialize SDL
 	if (SDL_Init(SDL_INIT_VIDEO) < 0) {
 		printf("SDL Init failed\n");
 		l_bReturn = 0;
 	}
 	else {
-		// if succeeded create our window
 		g_game.window = SDL_CreateWindow(title, x, y, w, h, flags);
-		// if the window creation succeeded create our renderer
 		if (g_game.window == 0) {
 			printf("Window creation failed\n");
 			l_bReturn = 0;
@@ -94,17 +91,13 @@ int initSDL(const char* title, const int x, const int y, const int w, const int 
 }
 
 void handleEvents() {
-	// message processing loop
 	SDL_Event event;
 	while (SDL_PollEvent(&event)) {
-		// check for messages
 		switch (event.type) {
-			// exit if the window is closed
 			case SDL_QUIT:
 				game_setFlag(&g_game, FLAG_DONE);
 				break;
 
-			// check for keypresses
 			case SDL_KEYDOWN:
 				switch (g_game.iState) {
 					case STATE_MAINMENU:
@@ -124,8 +117,7 @@ void handleEvents() {
 				}
 				break;
 		}
-		// end switch
-	} // end of message processing
+	}
 }
 
 void update() {
@@ -155,7 +147,6 @@ void render() {
 		multiplayer_setup_state_render(&g_game);
 	}
 
-	// Render the rect to the screen
 	SDL_RenderPresent(g_game.renderer);
 }
 
