66b9fbf6827a01728db76a4edb270ca704708a4e
Ghislain Rodrigues <git@ghislain-rodrigues.fr>
2016-11-28 07:35:23 +0000

    identation

--- net.c
+++ net.c
@@ -15,24 +15,24 @@ char net_getNextIP(struct ifaddrs **ifap, char **interface, char **addr) {
 	*interface = 0;
 	*addr = 0;
 
-	if (*ifap) {
-		if (
-			(*ifap)->ifa_addr->sa_family != AF_INET
-			|| IFF_LOOPBACK == ((*ifap)->ifa_flags & IFF_LOOPBACK)
-		) {
-			*ifap = (*ifap)->ifa_next;
-			return net_getNextIP(ifap, interface, addr);
-		}
-
-		sa = (struct sockaddr_in *) (*ifap)->ifa_addr;
-		*interface = (*ifap)->ifa_name;
-		*addr = inet_ntoa(sa->sin_addr);
-		*ifap = (*ifap)->ifa_next;
-
-		return 1;
+	if (!*ifap) {
+		return 0;
 	}
 
-	return 0;
+	if (
+		(*ifap)->ifa_addr->sa_family != AF_INET
+		|| IFF_LOOPBACK == ((*ifap)->ifa_flags & IFF_LOOPBACK)
+	) {
+		*ifap = (*ifap)->ifa_next;
+		return net_getNextIP(ifap, interface, addr);
+	}
+
+	sa = (struct sockaddr_in *) (*ifap)->ifa_addr;
+	*interface = (*ifap)->ifa_name;
+	*addr = inet_ntoa(sa->sin_addr);
+	*ifap = (*ifap)->ifa_next;
+
+	return 1;
 }
 
 void net_freeIfAddr(struct ifaddrs *ifap) {
